8c5ebd974dda8805a99c2cdd9882e9fd835de6f2
pixls <tha.pixls@gmail.com>
2022-09-27 14:06:51 -0400

    set up skeleton
    made empty components for dogdetails and doglist, add default dog prop

--- dog_finder/src/App.js
+++ dog_finder/src/App.js
@@ -1,25 +1,69 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Redirect, Switch } from 'react-router-dom';
+import DogList from './DogList';
+import DogDetails from './DogDetails';
 
-function App() {
+
+function App({dogs}) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path="/dogs" >
+          <DogList dogs={dogs} />
+        </Route>
+        <Route path="/dogs/:name" >
+          <DogDetails dog={dog} />
+        </Route>
+        <Redirect to="/dogs" />
+      </Switch>
     </div>
   );
+}
+
+App.defaultProps = {
+  dogs: [
+    {
+      name: "Whiskey",
+      age: 5,
+      src: whiskey,
+      facts: [
+        "Whiskey loves eating popcorn.",
+        "Whiskey is a terrible guard dog.",
+        "Whiskey wants to cuddle with you!"
+      ]
+    },
+    {
+      name: "Duke",
+      age: 3,
+      src: duke,
+      facts: [
+        "Duke believes that ball is life.",
+        "Duke likes snow.",
+        "Duke enjoys pawing other dogs."
+      ]
+    },
+    {
+      name: "Perry",
+      age: 4,
+      src: perry,
+      facts: [
+        "Perry loves all humans.",
+        "Perry demolishes all snacks.",
+        "Perry hates the rain."
+      ]
+    },
+    {
+      name: "Tubby",
+      age: 4,
+      src: tubby,
+      facts: [
+        "Tubby is really stupid.",
+        "Tubby does not like walks.",
+        "Angelina used to hate Tubby, but claims not to anymore."
+      ]
+    }
+  ]
 }
 
 export default App;
